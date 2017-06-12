9cb526864aba569e9c66ac4a0bc02b74efbbd60c
charlie <the_charlie_daly@hotmail.co.uk>
2017-06-12 22:27:03 +0100

    Reducer tests.

--- spec/reducer.spec.ts
+++ spec/reducer.spec.ts
@@ -1,15 +1,50 @@
-import {createReducer, Handler} from "../src/reducers";
+import {createReducer, combineReducers, Handler} from "../src/reducers";
 import {MockReducerDescriptor} from "./mocks";
 
-describe("Reducer", () => {
+describe("combineReducers", () => {
+    it("combines singly nested reducers", () => {
+        var fooReducer = new MockReducerDescriptor();
+        var barReducer = new MockReducerDescriptor();
+
+        var combined = combineReducers({
+            foo: fooReducer,
+            bar: barReducer
+        });
+
+        expect(Object.keys(combined)).toEqual(["foo", "bar"]);
+        expect(combined["foo"]).toEqual(fooReducer);
+        expect(combined["bar"]).toEqual(barReducer);
+    })
+
+    it("combines multi nested reducers", () => {
+        var fooReducer = new MockReducerDescriptor();
+        var barReducer = new MockReducerDescriptor();
+        var gooReducer = new MockReducerDescriptor();
+
+        var combined = combineReducers({
+            foo: fooReducer,
+            bar: barReducer,
+            goo: {
+                foo: gooReducer
+            }
+        });
+
+        expect(Object.keys(combined)).toEqual(["foo", "bar", "goo"]);
+        expect(combined["foo"]).toEqual(fooReducer);
+        expect(combined["bar"]).toEqual(barReducer);
+        expect(combined["goo"]["foo"]).toEqual(gooReducer);
+    })
+})
+
+describe("createReducer", () => {
+    const action = {type: "type", userId: "userid"};
     const testHandler: Handler = (reducer, action, uri) => {
         reducer(action, "state");
     }
     
-    it("dispatchs an action to a simple handler", () => {
+    it("creates simple reducer", () => {
         var mock = new MockReducerDescriptor();
-        const dispatch = createReducer(mock)(testHandler);
-        const action = {type: "type", userId: "userid"};
+        var dispatch = createReducer(mock)(testHandler);
 
         dispatch(action);
 
@@ -17,5 +52,50 @@ describe("Reducer", () => {
         expect(mock.actions[0].type).toEqual("type");
         expect(mock.actions[0].userId).toEqual("userid");
         expect(mock.states[0]).toEqual("state");
+    })
+
+    it("creates composite reducer", () => {
+        var foo = new MockReducerDescriptor();
+        var bar = new MockReducerDescriptor();
+        var reducer = combineReducers({
+            foo: foo,
+            bar: bar
+        });
+        var dispatch = createReducer(reducer)(testHandler);
+
+        dispatch(action);
+
+        expect(foo.actions[0]).toBeDefined();
+        expect(bar.actions[0]).toBeDefined();
+    })
+
+    it("creates composite reducer with unhandled", () => {
+        var foo = new MockReducerDescriptor();
+        var bar = new MockReducerDescriptor(undefined, false);
+        var reducer = combineReducers({
+            foo: foo,
+            bar: bar
+        });
+        var dispatch = createReducer(reducer)(testHandler);
+
+        dispatch(action);
+
+        expect(foo.actions[0]).toBeDefined();
+        expect(bar.actions[0]).toBeUndefined();
+    })
+
+    it("creates composite reducer with events filtered", () => {
+        var foo = new MockReducerDescriptor([action.type]);
+        var bar = new MockReducerDescriptor([], false);
+        var reducer = combineReducers({
+            foo: foo,
+            bar: bar
+        });
+        var dispatch = createReducer(reducer)(testHandler);
+
+        dispatch(action);
+
+        expect(foo.actions[0]).toBeDefined();
+        expect(bar.actions[0]).toBeUndefined();
     })
 })
